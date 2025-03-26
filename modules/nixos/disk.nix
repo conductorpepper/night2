e1dde5ea5266f1d2efaede2edb6d4d6cfdb4eb75
conductorpepper <100497642+conductorpepper@users.noreply.github.com>
2025-03-25 21:23:36 -0400

    i don't know at this point

--- modules/nixos/disk.nix
+++ modules/nixos/disk.nix
@@ -29,64 +29,84 @@ in {
     # two-space indent my beloved
     # https://github.com/nix-community/disko/blob/master/example/luks-btrfs-subvolumes.nix
     disko.devices = {
-      disk = {
-        main = {
-          type = "disk";
-          device = cfg.device;
-          content = {
-            type = "gpt";
-            partitions = {
-              ESP = {
-                size = "512M";
-                type = "EF00";
-                content = {
-                  type = "filesystem";
-                  format = "vfat";
-                  mountpoint = "/boot";
-                  mountOptions = ["umask=0077"];
-                };
+      disk.main = {
+        type = "disk";
+        inherit (cfg) device;
+        content = {
+          type = "gpt";
+          partitions = {
+            esp = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = ["umask=0077"];
               };
-              luks = {
-                size = "100%";
+            };
+            root = {
+              name = "root";
+              size = "100%";
+              content = {
+                type = "lvm_pv";
+                vg = "root_vg";
+              };
+            };
+          };
+        };
+      };
+
+      lvm_vg = {
+        root_vg = {
+          type = "lvm_vg";
+          lvs = {
+            root = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "crypted";
+                passwordFile = "/tmp/secret.key";
+                settings = {
+                  allowDiscards = true;
+                };
                 content = {
-                  type = "luks";
-                  name = "crypted";
-                  passwordFile = "/tmp/secret.key";
-                  settings = {
-                    allowDiscards = true;
-                  };
-                  content = {
-                    type = "btrfs";
-                    extraArgs = ["-f"];
-                    subvolumes = {
-                      "@" = {
-                        mountpoint = "/";
-                        mountOptions = ["compress=zstd" "noatime"];
-                      };
-                      "@home" = {
-                        mountpoint = "/home";
-                        mountOptions = ["compress=zstd" "noatime"];
-                      };
-                      "@nix" = {
-                        mountpoint = "/nix";
-                        mountOptions = ["compress=zstd" "noatime"];
-                      };
-                      "@cache" = {
-                        mountpoint = "/var/cache";
-                        mountOptions = ["compress=zstd" "noatime"];
-                      };
-                      "@log" = {
-                        mountpoint = "/var/log";
-                        mountOptions = ["compress=zstd" "noatime"];
-                      };
-                      "@tmp" = {
-                        mountpoint = "/var/tmp";
-                        mountOptions = ["compress=zstd" "noatime"];
-                      };
-                      "@swap" = {
-                        mountpoint = "/.swapvol";
-                        swap.swapfile.size = "20M";
-                      };
+                  type = "btrfs";
+                  extraArgs = ["-f"];
+                  subvolumes = let
+                    mountOptions = ["compress=zstd" "noatime"];
+                  in {
+                    "@" = {
+                      mountpoint = "/";
+                      inherit mountOptions;
+                    };
+                    "@home" = {
+                      mountpoint = "/home";
+                      inherit mountOptions;
+                    };
+                    "@nix" = {
+                      mountpoint = "/nix";
+                      inherit mountOptions;
+                    };
+                    "@cache" = {
+                      mountpoint = "/var/cache";
+                      inherit mountOptions;
+                    };
+                    "@log" = {
+                      mountpoint = "/var/log";
+                      inherit mountOptions;
+                    };
+                    "@tmp" = {
+                      mountpoint = "/var/tmp";
+                      inherit mountOptions;
+                    };
+                    "@persist" = {
+                      mountpoint = "/persist";
+                      inherit mountOptions;
+                    };
+                    "@swap" = {
+                      mountpoint = "/.swapvol";
+                      swap.swapfile.size = "20M";
                     };
                   };
                 };
