8ae69b10c10002bd576ce7e2fbd767c812973779
conductorpepper <100497642+conductorpepper@users.noreply.github.com>
2025-04-07 18:01:15 -0400

    add network manager to prelude

--- configurations/nixos/prelude/default.nix
+++ configurations/nixos/prelude/default.nix
@@ -17,6 +17,8 @@
   nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
+  networking.networkmanager.enable = true;
+
   environment.systemPackages = with pkgs; [
     # TODO: add bin path
     (writeShellApplication {
