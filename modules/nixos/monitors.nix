430098177446e08d38c2c52226ab5ad4cac43583
ri <100497642+conductorpepper@users.noreply.github.com>
2025-04-22 18:33:55 -0400

    fix

--- modules/nixos/monitors.nix
+++ modules/nixos/monitors.nix
@@ -2,7 +2,6 @@
   options.utils.monitors = {
     monitors = lib.mkOption {
       description = "List of monitors";
-      longDescription = "List of monitors. The first monitor is considered primary.";
       type = lib.types.listOf lib.types.str;
       default = [];
     };
