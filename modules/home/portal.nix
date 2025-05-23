7a73010568b9780213f5e14b804f646209fadc16
ri <100497642+conductorpepper@users.noreply.github.com>
2025-05-23 10:27:56 -0400

    fix typo

--- modules/home/portal.nix
+++ modules/home/portal.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   xdg.portal.enable = true;
   xdg.portal.extraPortals = with pkgs; [
-    xdg-xdg-desktop-portal-gtk
+    xdg-desktop-portal-gtk
   ];
   xdg.portal.config = {
     common = {
