92bc1f01a470e7993a2b928ade8bc33ea8cd1a45
ri <100497642+conductorpepper@users.noreply.github.com>
2025-05-23 10:26:02 -0400

    portal

--- modules/home/portal.nix
+++ modules/home/portal.nix
@@ -1,6 +1,11 @@
 {pkgs, ...}: {
   xdg.portal.enable = true;
-    xdg.portal.extraPortals = with pkgs; [
-      xdg-xdg-desktop-portal-gtk
-    ];
+  xdg.portal.extraPortals = with pkgs; [
+    xdg-xdg-desktop-portal-gtk
+  ];
+  xdg.portal.config = {
+    common = {
+      default = ["hyprland" "gtk"];
+    };
+  };
 }
