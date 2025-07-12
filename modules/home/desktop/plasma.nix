b05489aa8ff410919611d1709556fcdbdd674c34
ri <pepper.network@tutanota.de>
2025-07-12 19:55:07 -0400

    konsole

--- modules/home/desktop/plasma.nix
+++ modules/home/desktop/plasma.nix
@@ -1,6 +1,27 @@
-{ flake, ... }:
+{
+  flake,
+  lib,
+  config,
+  ...
+}:
 {
   imports = [
     flake.inputs.plasma-manager.homeManagerModules.plasma-manager
   ];
+
+  programs.konsole = {
+    enable = true;
+    defaultProfile = "Nushell";
+    profiles = {
+      "Nushell" = {
+        command = lib.getExe config.programs.nushell.package;
+      };
+      "Zsh" = {
+        command = lib.getExe config.programs.zsh.package;
+      };
+      "Anime" = {
+        command = "ani-cli";
+      };
+    };
+  };
 }
--- modules/home/fonts.nix
+++ modules/home/fonts.nix
@@ -2,33 +2,37 @@
   flake,
   pkgs,
   ...
-}: let
+}:
+let
   inherit (flake) inputs;
-in {
-  home.packages = let
-    system = pkgs.stdenv.hostPlatform.system;
+in
+{
+  home.packages =
+    let
+      system = pkgs.stdenv.hostPlatform.system;
 
-    used-nerd-fonts = with pkgs.nerd-fonts; [
-      jetbrains-mono
-    ];
+      used-nerd-fonts = with pkgs.nerd-fonts; [
+        jetbrains-mono
+      ];
 
-    lemon-fonts = with inputs.nix-lemons.packages.${system}; [
-      middle-mann-fonts
-    ];
-  in
+      lemon-fonts = with inputs.nix-lemons.packages.${system}; [
+        middle-mann-fonts
+      ];
+    in
     with pkgs;
-      [
-        # fonts
-        roboto
-        noto-fonts
-        noto-fonts-cjk-sans
-        noto-fonts-cjk-serif
-        inter
-        commit-mono
-        cozette
-        tewi-font
-        terminus_font
-      ]
-      ++ used-nerd-fonts
-      ++ lemon-fonts;
+    [
+      # fonts
+      roboto
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      inter
+      commit-mono
+      cozette
+      tewi-font
+      terminus_font
+      hack-font
+    ]
+    ++ used-nerd-fonts
+    ++ lemon-fonts;
 }
