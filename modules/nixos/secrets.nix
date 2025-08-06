a024b2022e925a6aee80627e81d7c01942f41fc9
ri <pepper.network@tutanota.de>
2025-08-06 14:29:22 -0400

    secrets

--- modules/nixos/secrets.nix
+++ modules/nixos/secrets.nix
@@ -1,9 +1,13 @@
-{ flake, ... }:
+{ flake, pkgs, ... }:
 let
   inherit (flake) inputs;
 in
 {
   imports = [
     inputs.sops-nix.nixosModules.sops
+  ];
+
+  environment.systemPackages = with pkgs; [
+    sops
   ];
 }
