dbffd7801e4c654b2db1570d753e604c88b5ef9c
ri <pepper.network@tutanota.de>
2025-08-06 14:41:09 -0400

    leave it here for right now (#10)

--- modules/nixos/secrets.nix
+++ modules/nixos/secrets.nix
@@ -1,11 +1,21 @@
 { flake, pkgs, ... }:
 let
-  inherit (flake) inputs;
+  inherit (flake) inputs config;
+  inherit (inputs) self;
 in
 {
   imports = [
     inputs.sops-nix.nixosModules.sops
   ];
+
+  sops.defaultSopsFile = "${self}/secrets/secrets.yaml";
+  sops.defaultSopsFormat = "yaml";
+  sops.age.keyFile = "/home/${config.me.username}/.config/sops/age/keys.txt";
+  sops.secrets = {
+    example-key = { };
+    "myservice/my_subdir/my_secret" = { };
+    "anki/secret" = { };
+  };
 
   environment.systemPackages = with pkgs; [
     sops
